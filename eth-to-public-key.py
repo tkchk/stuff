6b86e09716140850faa48a4c88361401509dd1d1
Alex Tkachuk <alex@tkachuk.pp.ua>
2025-03-27 14:09:11 -0400

    changes

--- eth-to-public-key.py
+++ eth-to-public-key.py
@@ -1,9 +1,7 @@
-#eff415edb6331f4f67bdb7f1ecc639da9bcc0550b100bb275c7b5b21ce3a7804
-
 from eth_keys import keys
 from eth_utils import to_checksum_address
 
-private_key_hex = "eff415edb6331f4f67bdb7f1ecc639da9bcc0550b100bb275c7b5b21ce3a7804"
+private_key_hex = "6d79b9a9a0eb1d252859fd15a6287d4434b4926b7360650e0b3869e5e463f7d2"
 private_key = keys.PrivateKey(bytes.fromhex(private_key_hex))
 
 public_key = private_key.public_key
